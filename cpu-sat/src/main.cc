e0017a7cc1c9253311bd51388fa69d7a0a8fdfb7
aleksandar.radojicic <aleksandar.radojicic@epita.fr>
2021-06-21 01:09:21 +0200

    cpu-sat: add program option to choose solver

--- cpu-sat/src/main.cc
+++ cpu-sat/src/main.cc
@@ -2,27 +2,30 @@
 #include <iostream>
 
 #include "cnf.hh"
-#include "dpll.hh"
+#include "solver.hh"
 
 int main(int argc, char** argv)
 {
-    if (argc != 2)
+    if (argc < 2 || argc > 3)
+    {
+        std::cerr << "Usage: cpu-sat file.cnf [-v1|-v2|-v3]\n";
         return 1;
+    }
 
     std::string path = argv[1];
 
     auto cnf = Cnf(std::ifstream(path));
 
-    // cnf.dump(std::cout);
+    std::optional<Cnf::solution> res;
 
-    // std::cout << cnf.satisfies({true, true, true}) << std::endl;
-    // std::cout << cnf.satisfies({false, true, true}) << std::endl;
-
-    /*     cnf.unit_propagation(); */
-    /*  */
-    /*     cnf.dump(std::cout); */
-
-    auto res = Dpll::solve(cnf);
+    if (argc == 2)
+        res = Solver::solve_v3(cnf);
+    else if (std::string(argv[2]) == "-v1")
+        res = Solver::solve_v1(cnf);
+    else if (std::string(argv[2]) == "-v2")
+        res = Solver::solve_v2(cnf);
+    else
+        res = Solver::solve_v3(cnf);
 
     if (res.has_value())
         std::cout << "sat\n" << *res << "\n";
