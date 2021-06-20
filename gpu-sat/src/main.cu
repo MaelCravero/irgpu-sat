98c2e9ee750fd9961ceea1a68b2c9b8ddc32dc9c
aleksandar.radojicic <aleksandar.radojicic@epita.fr>
2021-06-21 01:44:37 +0200

    gpu-sat: add program option to choose solver version

--- gpu-sat/src/main.cu
+++ gpu-sat/src/main.cu
@@ -2,19 +2,32 @@
 #include <iostream>
 
 #include "cnf.cuh"
-#include "dpll.cuh"
+#include "solver.cuh"
 
 int main(int argc, char** argv)
 {
     using namespace host;
-    if (argc != 2)
+    if (argc < 2 || argc > 3)
+    {
+        std::cerr << "Usage: cpu-sat file.cnf [-v1|-v2|-v3]\n";
         return 1;
+    }
 
     std::string path = argv[1];
 
     auto cnf = Cnf(std::ifstream(path));
 
-    auto solution = dpll_solve(cnf);
+    std::optional<solution> solution;
+
+    if (argc == 2)
+        solution = solve_v3(cnf);
+
+    else if (std::string(argv[2]) == "-v1")
+        solution = solve_v1(cnf);
+    else if (std::string(argv[2]) == "-v2")
+        solution = solve_v2(cnf);
+    else
+        solution = solve_v3(cnf);
 
     if (solution.has_value())
         std::cout << "sat\n" << *solution << "\n";
